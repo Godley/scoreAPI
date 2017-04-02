269f6e101cc4679a915664499386eb1898ad7ea6
Godley <charlotte.godley@hotmail.co.uk>
2017-04-02 15:15:35 +0100

    updated models for each entry type

--- scores/admin.py
+++ scores/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
-
+from scores.models import Competition, Judge, Run, LineFollowing, StraightLineSpeed, MinimalMaze, MazeRun, StraightLineRun, GolfRun, BowlRun, Obstacle, Bowling, ObstacleCourse, LineFollowRun
 # Register your models here.
+
+class RunInline(admin.StackedInline):
+    model = Run
+
+
+class MazeRunInline(admin.StackedInline):
+    model = MazeRun
+
+
+class StraightLineInline(admin.StackedInline):
+    model =StraightLineRun
+
+
+class GolfRunInline(admin.StackedInline):
+    model = GolfRun
+
+
+class BowlRunInline(admin.StackedInline):
+    model = BowlRun
+
+
+class ObstacleInline(admin.StackedInline):
+    model = Obstacle
+
+
+class LineFollowInline(admin.StackedInline):
+    model = LineFollowRun
+
+
+class LineFollowAdmin(admin.ModelAdmin):
+    inlines = [LineFollowInline]
+
+
+class StraightLineAdmin(admin.ModelAdmin):
+    inlines = [StraightLineInline]
+
+
+class MinimalMazeAdmin(admin.ModelAdmin):
+    inlines = [MazeRunInline]
+
+
+class BowlingAdmin(admin.ModelAdmin):
+    inlines = [BowlRunInline]
+
+
+
+admin.site.register(Competition)
+admin.site.register(Judge)
+admin.site.register(LineFollowing, LineFollowAdmin)
+admin.site.register(StraightLineSpeed, StraightLineAdmin)
+admin.site.register(MinimalMaze, MinimalMazeAdmin)
+admin.site.register(Bowling, BowlingAdmin)
--- scores/models.py
+++ scores/models.py
@@ -15,14 +15,30 @@ class Competition(models.Model):
     teams = models.ManyToManyField(Team)
 
 
-class LineFollowing(Competition):
+class Entry(models.Model):
+    team = models.ForeignKey(Team)
+
+
+class LineFollowing(Entry):
     pass
 
-class StraightLineSpeed(Competition):
+
+class StraightLineSpeed(Entry):
+    pass
+
+class Golf(Entry):
     pass
 
 
-class MinimalMaze(Competition):
+class Bowling(Entry):
+    pass
+
+
+class ObstacleCourse(Entry):
+    pass
+
+
+class MinimalMaze(Entry):
     sensors_used = models.BooleanField()
 
 
@@ -33,22 +49,39 @@ class Run(models.Model):
 
 
 class MazeRun(Run):
+    entry = models.ForeignKey(MinimalMaze)
     wall_touches = models.IntegerField()
     abandoned = models.BooleanField()
 
 
 class StraightLineRun(Run):
+    entry = models.ForeignKey(StraightLineSpeed)
     wall_runner = models.BooleanField()
 
 
 class GolfRun(Run):
+    entry = models.ForeignKey(Golf)
     ball_in_hole = models.BooleanField()
     ball_in_sand_hazard = models.BooleanField()
     ball_in_water_hazard = models.BooleanField()
     abandoned = models.BooleanField()
 
+
+class BowlRun(models.Model):
+    entry = models.ForeignKey(Bowling)
+    pins_bowl_one = models.IntegerField()
+    pins_bowl_two = models.IntegerField()
+    foul = models.BooleanField()
+    time = models.TimeField()
+
+
 class Obstacle(models.Model):
-    name = models.CharField()
+    entry = models.ForeignKey(ObstacleCourse)
+    name = models.CharField(max_length=20)
     attempted = models.BooleanField()
     rescued = models.BooleanField()
     skipped = models.BooleanField()
+
+
+class LineFollowRun(Run):
+    entry = models.ForeignKey(LineFollowing)
